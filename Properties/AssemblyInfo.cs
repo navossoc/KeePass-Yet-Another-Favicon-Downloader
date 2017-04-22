977fcee6f8500bf4e697ac1b86d8b455ca324af3
Rafael Cossovan <navossoc@gmail.com>
2017-04-22 10:21:55 -0300

    Minimal KeePass plugin.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YetAnotherFaviconDownloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yet another favicon downloader for KeePass 2.x")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YetAnotherFaviconDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Rafael Cossovan de França")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Rafael Cossovan de França")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- YetAnotherFaviconDownloaderExt.cs
+++ YetAnotherFaviconDownloaderExt.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using KeePass.Plugins;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace YetAnotherFaviconDownloader
 {
-    public class YetAnotherFaviconDownloaderExt
+    public class YetAnotherFaviconDownloaderExt : Plugin
     {
+        public override bool Initialize(IPluginHost host)
+        {
+            Log("Plugin Initialize");
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            Log("Plugin Terminate");
+            return;
+        }
+
+        private void Log(string format, params object[] args)
+        {
+            Debug.Print("[YAFD] " + format, args);
+        }
     }
 }
