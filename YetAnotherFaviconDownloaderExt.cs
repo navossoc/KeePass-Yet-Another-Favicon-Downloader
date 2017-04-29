6a2b5c01f3f20084633028c0b41bef5f85bd3aca
Rafael Cossovan <navossoc@gmail.com>
2017-04-28 23:57:19 -0300

    Adds a menu to remove all custom icons.

--- YetAnotherFaviconDownloaderExt.cs
+++ YetAnotherFaviconDownloaderExt.cs
@@ -1,4 +1,5 @@
 ﻿using KeePass.Plugins;
+using KeePassLib;
 using System;
 
 namespace YetAnotherFaviconDownloader
@@ -15,6 +16,10 @@ namespace YetAnotherFaviconDownloader
 
             var entriesMenu = m_host.MainWindow.EntryContextMenu.Items;
             entriesMenu.Add("Download favicon", null, MenuEntry_Click);
+#if DEBUG
+            // A little helper to make my tests easier
+            entriesMenu.Add("Clear all favicon", null, MenuEntry2_Click);
+#endif
 
             return true;
         }
@@ -34,6 +39,37 @@ namespace YetAnotherFaviconDownloader
             var downloader = new FaviconDownloader(m_host);
             downloader.Run(entries);
         }
+
+#if DEBUG
+        private void MenuEntry2_Click(object sender, EventArgs e)
+        {
+            Util.Log("Menu entry 2 clicked");
+
+            // Reset icons from all groups
+            var groups = m_host.Database.RootGroup.GetGroups(true);
+            foreach (var group in groups)
+            {
+                group.IconId = PwIcon.Folder;
+                group.CustomIconUuid = PwUuid.Zero;
+                group.Touch(true, false);
+            }
+
+            // Reset icons from all entries
+            var entries = m_host.Database.RootGroup.GetEntries(true);
+            foreach (var entry in entries)
+            {
+                entry.IconId = PwIcon.Key;
+                entry.CustomIconUuid = PwUuid.Zero;
+                entry.Touch(true, false);
+            }
+
+            // Remove all custom icons from database
+            m_host.Database.CustomIcons.Clear();
+
+            // Refresh icons
+            m_host.MainWindow.UpdateUI(false, null, true, null, true, null, true);
+        }
+#endif
 
         public override void Terminate()
         {
