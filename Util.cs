ee8fb30b5866aaee3ea33d75da9b0f8c2e48026d
Rafael Cossovan <navossoc@gmail.com>
2017-04-30 05:35:07 -0300

    Adds a method to obtain KeePass proxy configuration.

--- Util.cs
+++ Util.cs
@@ -1,9 +1,26 @@
-﻿using System.Diagnostics;
+﻿using System.Net;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace YetAnotherFaviconDownloader
 {
     public sealed class Util
     {
+        /// <summary>
+        /// Get Proxy configuration from KeePass.
+        /// </summary>
+        /// <returns>Returns a proxy instance.</returns>
+        /// <remarks>This is a little hacky, but they made me do it!</remarks>
+        public static IWebProxy GetKeePassProxy()
+        {
+            var parameters = new object[] { null };
+            var result = (bool)typeof(KeePassLib.Serialization.IOConnection)
+                ?.GetMethod("GetWebProxy", BindingFlags.Static | BindingFlags.NonPublic)
+                ?.Invoke(null, parameters);
+
+            return parameters[0] as IWebProxy;
+        }
+
         public static void Log(string format, params object[] args)
         {
             Debug.Print("[YAFD] " + format, args);
