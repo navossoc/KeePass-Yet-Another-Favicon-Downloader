dbbf191a12a23fe56e1c12e4657190a4a36b1476
eelke <eelke@eelkeklein.nl>
2021-03-07 04:26:30 -0300

    Add test project.
    Pull Request #54.
    
    Co-authored-by: Rafael Cossovan <navossoc@gmail.com>

---
+++ Tests/FaviconDownloaderTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YetAnotherFaviconDownloader;
+
+namespace Tests
+{
+    [TestClass]
+    public class FaviconDownloaderTest
+    {
+        [TestMethod]
+        public void TestCookieContainer()
+        {
+            // Eelke76
+            // https://github.com/navossoc/KeePass-Yet-Another-Favicon-Downloader/pull/54
+
+            var url = "https://mijn.ing.nl/internetbankieren/SesamLoginServlet";
+
+            using (FaviconDownloader fd = new FaviconDownloader())
+            {
+                // Download favicon
+                byte[] data = fd.GetIcon(url);
+                Assert.IsNotNull(data);
+            }
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("636ac514-50fb-4d60-b534-b42f663158ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
